5fa4678f4d1aef8e5bb267784306b70a151de4bd
clown-yoyo <maximehersent@gmail.com>
2016-11-03 14:59:46 +0100

    Create FolderMngmt.R

---
+++ FolderMngmt.R
@@ -0,0 +1,5 @@
+############# FOLDERS
+main.folder <- "C://Users//m-hersent//Desktop//BWM-REN2017//ASTY//3-POST_ANALYSIS"
+inputs.folder   <- paste(main.folder, "INPUTS", sep = "//")
+outputs.folder  <- paste(main.folder, "OUTPUTS", sep = "//")
+script.folder   <- paste(main.folder, "SCRIPTS", sep = "//")
